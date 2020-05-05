dd54607f6e7e9946555d8a445bdc83543c28a6c2
Amit Rajput <amitrajput123.ar@gmail.com>
2020-05-05 21:12:17 +0530

    removing unwanted logs

--- packages/Board/index.tsx
+++ packages/Board/index.tsx
@@ -14,10 +14,7 @@ interface Props {
 }
 
 export default function Board({ updatePin, boardJson }: Props) {
-
-  console.log(boardJson);
-
-  return (
+return (
     <div className="board_ui__board flex flex-wrap">
       {Object.values(boardJson.pins).sort((a: PinProps, b: PinProps) => parseInt(a.id) > parseInt(b.id) ? -1 : parseInt(a.id) < parseInt(b.id) ? 1 : 0 ).map((p: PinProps) => (
         <Pin key={p.id} pinProps={{...p}} onChange={updatePin} />
