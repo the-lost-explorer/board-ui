92de7ea6c9f4019ed98249c9f7bde7388dffe10a
ShreyasArun <2mydemonlord@gmail.com>
2020-05-05 16:35:42 +0530

    Moving logic to the stories

--- packages/Board/index.tsx
+++ packages/Board/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Pin, { PinProps } from '../Pin';
 
 export interface BoardProps {
@@ -8,13 +8,18 @@ export interface BoardProps {
   }
 }
 
-export default function Board({}: BoardProps) {
+interface Props {
+  boardJson: BoardProps,
+  updatePin: (p : PinProps) => void,
+}
+
+export default function Board({ updatePin, boardJson }: Props) {
+
+  console.log(boardJson);
 
   return (
     <div className="board_ui__board flex flex-wrap">
-      <button onClick={addPin}>New Note</button>
-
-      {Object.values(board.pins).sort((a: PinProps, b: PinProps) => parseInt(a.id) > parseInt(b.id) ? -1 : parseInt(a.id) < parseInt(b.id) ? 1 : 0 ).map((p: PinProps) => (
+      {Object.values(boardJson.pins).sort((a: PinProps, b: PinProps) => parseInt(a.id) > parseInt(b.id) ? -1 : parseInt(a.id) < parseInt(b.id) ? 1 : 0 ).map((p: PinProps) => (
         <Pin key={p.id} pinProps={{...p}} onChange={updatePin} />
       ))}
     </div>
